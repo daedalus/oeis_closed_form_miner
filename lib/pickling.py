8ca585c786ed108d63afe54240ab98f7b3d53215
dclavijo <clavijodario@gmail.com>
2023-12-29 19:02:35 -0300

    pickling safeguard

--- lib/pickling.py
+++ lib/pickling.py
@@ -2,11 +2,12 @@ import bz2
 import pickle
 import _pickle as cPickle
 import sys
-
+import os
 
 # Pickle a file and then compress it into a file with extension 
 def compress_pickle(filename, data):
  sys.stderr.write(f"saving pickle {filename}...\n")
+ os.system(f"cp {filename} {filename}.bkp")
  with bz2.BZ2File(filename, 'w') as f: 
    cPickle.dump(data, f)
 
