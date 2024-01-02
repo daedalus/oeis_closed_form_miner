3abecb05e05fbc60a2884fcc150185805b32a4b1
Darío <clavijodario@gmail.com>
2024-01-01 19:22:51 -0600

    fix pickling

--- lib/pickling.py
+++ lib/pickling.py
@@ -1,19 +1,20 @@
 import bz2
-import pickle
-import _pickle as cPickle
+import cPickle as pickle
 import sys
 import os
 
-# Pickle a file and then compress it into a file with extension 
+
+# Pickle a file and then compress it into a file with extension
 def compress_pickle(filename, data):
- sys.stderr.write(f"saving pickle {filename}...\n")
- os.system(f"cp {filename} {filename}.bkp")
- with bz2.BZ2File(filename, 'w') as f: 
-   cPickle.dump(data, f)
+    sys.stderr.write(f"saving pickle {filename}...\n")
+    os.system(f"cp {filename} {filename}.bkp")
+    with bz2.BZ2File(filename, "w") as f:
+        pickle.dump(data, f)
+
 
 # Load any compressed pickle file
 def decompress_pickle(filename):
- sys.stderr.write(f"loading pickle {filename}...\n")
- data = bz2.BZ2File(filename, 'rb')
- data = cPickle.load(data)
- return data
+    sys.stderr.write(f"loading pickle {filename}...\n")
+    data = bz2.BZ2File(filename, "rb")
+    data = pickle.load(data)
+    return data
